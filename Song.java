8983dcaa75abfa11ac9285c24a012aa0a8348ec1
Ethans1428 <Ethans1228@gmail.com>
2025-03-28 14:03:05 +0000

    Created the Song class

--- Song.java
+++ Song.java
@@ -1,3 +1,47 @@
+/**
+ * Represents a song object with its title, its artists name and its play count
+ */
 public class Song {
-    
+    private String songTitle; //name of the song
+    private String artistName; //name of the artist who made that song
+    private int playCount; //the number of times that that song has been played
+
+    /**
+     * The constructor for the song class
+     */
+    public Song(String songTitle, String artistName, int playCount) {
+        this.songTitle = songTitle;
+        this.artistName = artistName;
+        this.playCount = playCount;
+    }
+
+    /**
+     * Returns a string representation of the song so that it can be written to the songs.txt file
+     */
+    @Override
+    public String toString() {
+        return songTitle + "," + artistName + "," + playCount;
+    }
+
+    /**
+     * Gets the songs title
+     */
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    /**
+     * Gets the artists name
+     */
+    public String getArtistName() {
+        return artistName;
+    }
+
+    /**
+     * Gets the songs play count
+     */
+    public int getPlayCount() {
+        return playCount;
+    }
 }
+
